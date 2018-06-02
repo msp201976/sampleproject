0722d62fbe4e824ed3cad7d395d35d2cefbbb138
Mukund P <mukund.purandare@gmail.com>
2018-06-02 04:17:41 -0400

    adding a console loop

---
+++ src/main/java/mukundwork/consolegame/App.java
@@ -0,0 +1,36 @@
+package mukundwork.consolegame;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "**Welcome to the console Game!" );
+        System.out.println( "  Use Q to Quit" );
+        
+        
+   	 	Scanner in = new Scanner(System.in);      
+        while(true){
+	    	 
+	         System.out.println("Enter a string");
+	         String inputString = in.nextLine();
+	         
+	         if("Q".equalsIgnoreCase(inputString)){
+	        	 break;
+	         }
+	         
+	         processInput(inputString);
+        	
+        }
+    }
+    
+    private static void processInput(String inputString){
+    	System.out.println("echo: "+inputString);
+    	
+    }
+}
